fd12a9a579bc490b1ba6d6cd06a083c39083f276
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-10-08 22:29:50 +0200

    Update main.rs
    Create console clear function

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-
+fn clear() {
+    if cfg!(unix) {
+        std::process::Command::new("clear").status().unwrap();
+    } else if cfg!(windows) {
+        std::process::Command::new("cls").status().unwrap();
+    }
+}
