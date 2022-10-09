2269152b03592e0b444bbcf808ea1d1b6e9ce96d
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-10-09 20:00:55 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,3 +18,24 @@ fn clear() {
         std::process::Command::new("cls").status().unwrap();
     }
 }
+
+fn main() {
+    let mut new_game = true;
+    clear();
+    loop {
+        let input;
+        if new_game {
+            input = prompt("Welcome to the pendu game ! Start ? (y/n) > ");
+        } else {
+            input = prompt("Restart ? (y/n) > ");
+        }
+        if input == "y" || input == "o" || input.len() == 0 {
+            new_game = false;
+            start();
+        } else if input == "n" || input == "exit" || input == "quit" || input == "q" {
+            break;
+        } else {
+            clear();
+        }
+    }
+}
