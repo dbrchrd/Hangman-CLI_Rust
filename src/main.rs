3b2a1e92318f96e7f5479a9308bf873c30791f94
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-10-09 08:11:56 +0200

    Update main.rs
    Create prompt function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::Write;
+
+fn prompt(name: &str) -> String {
+    let mut line = String::new();
+    print!("{}", name);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin()
+        .read_line(&mut line)
+        .expect("Error: Could not read a line");
+
+    return line.trim().to_string();
+}
+
 fn clear() {
     if cfg!(unix) {
         std::process::Command::new("clear").status().unwrap();
